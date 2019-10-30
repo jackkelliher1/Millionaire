85dc33537b03a025b03dedc81703638fa9be9f89
jackkelliher1 <55121775+jackkelliher1@users.noreply.github.com>
2019-10-31 10:02:38 +1300

    Created code document and added 5 methods

---
+++ millionaireAssessment/millionaireAssessment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace millionaireAssessment
+{
+    class Program
+    {
+        static void Method1()
+        {
+
+        }
+
+        static void Method2()
+        {
+
+        }
+
+        static void Method3()
+        {
+
+        }
+
+        static void Method4()
+        {
+
+        }
+
+        static void Method5()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
