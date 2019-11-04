06dc163dcdad0c029451fb0d7882b3cc314993ba
jackkelliher1 <55121775+jackkelliher1@users.noreply.github.com>
2019-11-05 11:54:19 +1300

    Finished methods to 4, started on 5

--- millionaireAssessment/millionaireAssessment/Program.cs
+++ millionaireAssessment/millionaireAssessment/Program.cs
@@ -13,15 +13,26 @@ namespace millionaireAssessment
         public string lName;
         public string interest;
     }
+
+    struct Question
+    {
+        public string q;
+        public string tAnswer;
+        public string fAnswer1;
+        public string fAnswer2;
+        public string fAnswer3;
+    }
     class Program
     {
         //Main Method, creates student array and menu
         static void Main(string[] args)
         {
             bool exit = false;
-            int count = 0; //keeps track of studentArray pos
+            int count = 0, num, money; //keeps track of studentArray pos
+            Student player = new Student(); //saves the player data
             Student[] studentArray = new Student[30]; //All students stored
             Student[] finalists = new Student[10]; //10 Finalist students stored
+            Question[] questionList = new Question[10]; //Questions Stored
             StreamReader sr = new StreamReader(@"millionaire.txt"); //Reads from file
             while (!sr.EndOfStream) //Fills Array
             {
@@ -30,24 +41,46 @@ namespace millionaireAssessment
                 studentArray[count].interest = sr.ReadLine();
                 count++;
             }
-
+            sr.Close();
+            count = 0;
+            StreamReader sr1 = new StreamReader(@"questions.txt");
+            while (!sr1.EndOfStream)
+            {
+                questionList[count].q = sr1.ReadLine();
+                questionList[count].tAnswer = sr1.ReadLine();
+                questionList[count].fAnswer1 = sr1.ReadLine();
+                questionList[count].fAnswer2 = sr1.ReadLine();
+                questionList[count].fAnswer3 = sr1.ReadLine();
+            }
+            sr1.Close();
             while (!exit) //Menu System
             {
                 Console.Clear();
                 Console.Write("Who Wants to Be a Millionaire\n\t1.Contestants\n\t2.Update Interests\n\t3.Generate Finalists\n\t4.Select Player\n\t5.Play\n\nEnter the relevent number:");
-                switch (Convert.ToInt32(Console.ReadLine()))
-                {
-                    case 1:
-                        Method1(studentArray);
-                        Console.ReadLine();
-                        break;
-                    case 2:
-                        Method2(studentArray);
-                        break;
-                    case 3:
-                        Method3(studentArray, finalists);
-                        Console.ReadLine();
-                        break;
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num < 6 && num > 0) {
+                    switch (num)
+                    {
+                        case 1:
+                            Method1(studentArray);
+                            Console.ReadLine();
+                            break;
+                        case 2:
+                            Method2(studentArray);
+                            break;
+                        case 3:
+                            Method3(studentArray, finalists);
+                            Console.ReadLine();
+                            break;
+                        case 4:
+                            Method4(finalists, ref player);
+                            Console.ReadLine();
+                            break;
+                        case 5:
+                            Method5(questionList, player);
+                            Console.ReadLine();
+                            break;
+                    }
                 }
             }
         }
@@ -113,36 +146,77 @@ namespace millionaireAssessment
         //Generates 10 finalists
         static void Method3(Student[] array, Student[] finalists)
         {
-            Student temp;
+            bool repeat = false; //Variable for the status of a repeating Student
+            Student temp; //Temporary variable for storing student
             Random rand = new Random();
+
             Console.Clear();
-            for(int i = 0; i < finalists.Length; i++)
+            for(int i = 0; i < finalists.Length; i++) //Finds a unique student for each slot of the finalists array
             {
-                temp = array[rand.Next(30)];
-                for (int a = 0; a < finalists.Length; a++)
+                temp = array[rand.Next(30)]; //Finds a random student
+                for (int a = 0; a < finalists.Length; a++) //checks the student does not already exsist in the array
                 {
-                    if (temp.lName.CompareTo(array[a].lName) == 1)
+                    if (temp.lName.CompareTo(finalists[a].lName) == 0) //Checks to see whether the last name of temp is equal to any already in the finalists array
                     {
-                        i = i - 1;
+                        repeat = true; //Sets repeat to true if the student is a repeat
                     }
                 }
+                
+                if(!repeat) //assigns temp to finalists if it is not a repeated student.
+                {
+                    finalists[i] = temp;
+                }else
+                {
+                    i = i - 1;
+                }
+
+                repeat = false; //sets repeat back to false
             }
 
-            Console.WriteLine("First Name".PadRight(15) + "Last Name".PadRight(30) + "Interest".PadRight(15));
-            foreach (Student i in finalists)
-            {
-                Console.WriteLine(i.fName.PadRight(15) + i.lName.PadRight(30) + i.interest.PadRight(15));
-            }
+            Method1(finalists); //Uses method1 to write the 10 finalists to screen.
         }
 
-        /* static void Method4()
+        static void Method4(Student[] finalists, ref Student player) //Randomly select a student to be the player
         {
-
+            Random rand = new Random();
+            player = finalists[rand.Next(10)];
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(player.fName + " " + player.lName + " was selected as the player.");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\nPress enter to return to menu");
         }
 
-        static void Method5()
+        static void Method5(Question[] array, Student player)
         {
+            int[] prizes = {100, 200, 300, 500, 1000, 2000, 4000, 8000, 16000, 32000, 64000, 125000, 250000, 500000, 1000000};
+            int count = 0, pos = 0;
+            bool exit = false;
+            // do
+            // {
+                Question question = new Question();
+                Random rand = new Random();
+                foreach (int i in prizes)
+                {
+                    if (count == pos)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
 
-        } */
+                    Console.Write("$" + i + "  ");
+
+                    if (count == pos)
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+
+                    count++;
+                }
+
+                Console.WriteLine("You are playing as " + player.fName + " " + player.lName);
+                question = array[rand.Next(10)];
+                Console.WriteLine(question.q);
+            // } while (!exit);
+        }
     }
 }
